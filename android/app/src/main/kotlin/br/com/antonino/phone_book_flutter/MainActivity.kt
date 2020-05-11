802cd9f228f75f6e2bad8604150e970302dd2879
Matheus Antonino <matrajano12@gmail.com>
2020-05-10 22:38:29 -0300

    First commit

---
+++ android/app/src/main/kotlin/br/com/antonino/phone_book_flutter/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.antonino.phone_book_flutter
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
