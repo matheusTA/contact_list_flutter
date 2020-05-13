d6a2d3f14292b4ae7e5e8b7326d2898e9b5d8f17
Matheus Antonino <matrajano12@gmail.com>
2020-05-13 20:21:34 -0300

    Update function main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:contact_list_flutter/src/screens/contact_screen.dart';
 import 'package:contact_list_flutter/src/screens/home_screen.dart';
 
 void main() => runApp(MaterialApp(
-      home: ContactScrenn(),
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
     ));
